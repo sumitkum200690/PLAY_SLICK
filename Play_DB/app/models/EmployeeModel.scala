1ad95be5b59e24dd31a85de753e4df14d6624047
Prakash Suvarnkar <prakashs@evolvingsols.com>
2015-09-30 15:11:13 +0530

    cahnged with css and UI 2nd stable working code with updated DB

--- Play_DB/app/models/EmployeeModel.scala
+++ Play_DB/app/models/EmployeeModel.scala
@@ -2,7 +2,7 @@
 
 package models
 
-class EmployeeModel(val id: Long, val name: String, val departmentId: Long, val isManager: Boolean,val departmentName: String, val current_project: String, val email: String, val contact_number: Long, val location: String)
+case class EmployeeModel(val id: Long, val name: String, val departmentId: Long, val isManager: Boolean,val departmentName: String, val current_project: String, val email: String, val contact_number: Long, val location: String)
                     
                     
                     
